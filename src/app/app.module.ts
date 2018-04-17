547ec1c854a28f3a832656d5fc0288d4dfee5e18
José Raul de Quadros <jose.raul@univille.br>
2018-04-17 16:14:44 -0300

    Teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import {TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    PainelComponent,
+    TentativasComponent
   ],
   imports: [
     BrowserModule
